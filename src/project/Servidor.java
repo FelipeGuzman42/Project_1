152ae9529e611f528af4f3adfa61e4cb0cbe7ef4
98mbohra <49011759+98mbohra@users.noreply.github.com>
2020-02-12 21:31:11 -0500

    Added increment and logic using buffer methods

--- src/project/Servidor.java
+++ src/project/Servidor.java
@@ -2,8 +2,35 @@ package project;
 
 public class Servidor extends Thread{
 	private int servidor_count = 0;
+        private Mensaje men;
+        private static Buffer br;
 	
 	public Servidor(String contador) {
 		servidor_count = Integer.parseInt(contador);
 	}
+        
+        public void run() {
+            
+            br.P(this);
+            men = br.receiveMessage();
+            incrementer(men);
+            br.returnMessage(men);
+            br.V(this);
+	}
+        
+        /**
+         * Incrementer
+         *
+         * @param men 
+         * 
+         * gets the number stored in Mensaje class from the buffer,
+         * increments and updates in the mensaje class to be returned.
+         */
+        public void incrementer (Mensaje men){
+            
+            int temp;
+            temp = men.getNumber();
+            men.setNumber(temp++);
+            
+        }
 }
