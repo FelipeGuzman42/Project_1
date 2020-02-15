ccca5219dddd31aaff0206f3ae15ec2db05d6df1
98mbohra <49011759+98mbohra@users.noreply.github.com>
2020-02-14 19:26:45 -0500

    Added buffer as input to server, and prints for testing

--- src/project/Servidor.java
+++ src/project/Servidor.java
@@ -1,17 +1,18 @@
-package project;
-
 public class Servidor extends Thread{
 	private int servidor_count = 0;
         private Mensaje men;
         private static Buffer br;
 	
-	public Servidor(String contador) {
+	public Servidor(String contador, Buffer br) {
 		servidor_count = Integer.parseInt(contador);
+                this.br = br; 
+                
 	}
         
         public void run() {
-            
+            System.out.println("Server number" +servidor_count+ "Started");
             br.P(this);
+            System.out.println("Resource requested");
             men = br.receiveMessage();
             incrementer(men);
             br.returnMessage(men);
